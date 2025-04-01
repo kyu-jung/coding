a8c30840d378c0ca0c668d22464ab7820ae87081
kyu-jung <bongseo1012@gmail.com>
2025-04-01 16:19:28 +0900

    Add files via upload

---
+++ char_str/char_str/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace char_str
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //문자형 자료형 공부
+            char a = 'A';
+            Console.WriteLine("첫"+ a);
+
+            //string 자료형 공부
+            string hello = "Hello Asan";
+            Console.WriteLine("안녕 아산 :" + hello);
+
+            //string 연결하기 공부
+            string name = "최규정";
+            string check = " 첫 수업 출석";
+            Console.WriteLine(name + check);
+
+            //string 길이 알아보기
+            int len = name.Length;
+            Console.WriteLine(len);
+
+            //string 글자 자르기
+            string sub = hello.Substring(0, 2);
+            Console.WriteLine(sub);
+
+
+            //string 글자 대체하기
+            string rephello = hello.Replace("Asan", "Seoul");
+            Console.WriteLine(rephello);
+
+            //string 글자 잘라서 표현하기
+            string data = "사과,바나나,오렌지";
+            string[] fruitlist = data.Split(',');
+
+            foreach (var item in fruitlist)
+            {
+                Console.WriteLine("과일 : " + item);
+            }
+
+            //문장안에 문장 작은 따옴표편 ('')
+            string longstr = "안녕하세요 오늘은 'C# 기초'를 수업하고 있습니다";
+            Console.WriteLine(longstr);
+
+            //문장안에 문장 탈출문자편 (\)
+            string longstr2 = "안녕하세요 오늘은 \"C# 기초\"를 수업하고 있습니다";
+            Console.WriteLine(longstr2);
+
+        }
+    }
+}
---
+++ control/control/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // if 문
+            // 주어진 조건이 true일때 코드 실행. 조건이 false이면 실행되지 않고 넘어감
+            int 나이 = 20;
+            if (나이 > 20)
+            {
+                Console.WriteLine("성인입니다.");
+            }
+            
+            
+            // else if 문
+            // 조건이 많은경우 갯수를 늘릴수 있음.
+            else if (나이 == 20)
+            {
+                Console.WriteLine("20살 입니다.");
+            }
+
+           
+            // if - else 문
+            else
+            {
+                Console.WriteLine("20살이 아닙니다.");
+            }
+
+
+
+            // 점수 85점을 초기화 합니다.
+            // 90점이상은 "A학점", 80점 이상은 "B학점", 70점 이상은 "C학점" 나머지는 "D학점"
+            int 점수 = 85;
+            if (점수 >= 90)
+            {
+                Console.WriteLine("A학점");
+            }
+            else if (점수 >= 80)
+            {
+                Console.WriteLine("B학점");
+            }
+            else if (점수 >= 70)
+            {
+                Console.WriteLine("C학점");
+            }
+            else
+            {
+                Console.WriteLine("D학점");
+            }
+
+
+            // switch (변수) {case :    출력;   break;     default :   출력 ;      break ;}
+            string 요일 = "토요일";
+            switch (요일)
+            {
+                case "월요일":
+                    Console.WriteLine("월요일입니다");
+                    break;
+                case "화요일":
+                    Console.WriteLine("화요일입니다");
+                    break;
+                case "수요일":
+                    Console.WriteLine("수요일입니다");
+                    break;
+                case "목요일":
+                    Console.WriteLine("목요일입니다");
+                    break;
+                case "금요일":
+                    Console.WriteLine("금요일입니다");
+                    break;
+                default:
+                    Console.WriteLine("주말입니다");
+                    break;
+            }
+
+
+            // 무지개 초기화는 마음에 드는 색
+            // case 빨 주 노 초 파, default 남 보
+            // Console.WriteLine("빨간색입니다");
+            // Console.WriteLine("남색 또는 보라색입니다");
+
+
+            // switch 문에서 goto사용    -> 특정 case로 이동.
+            string 무지개 = "노란색";
+            switch (무지개)
+            {
+                case "빨간색":
+                    Console.WriteLine("빨간색입니다");
+                    break;
+                case "주황색":
+                    Console.WriteLine("주황색입니다");
+                    break;
+                case "노란색":
+                    Console.WriteLine("노란색입니다");
+                    goto case "파란색";
+                    break;
+                case "초록색":
+                    Console.WriteLine("초록색입니다");
+                    break;
+                case "파란색":
+                    Console.WriteLine("파란색입니다");
+                    break;
+                default:
+                    Console.WriteLine("남색 또는 보라색입니다");
+                    break;
+            }
+
+           
+
+
+
+        }
+    }
+}
