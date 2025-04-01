bdc9dae1090ea077514ef7be35eae492e3e28439
kyu-jung <bongseo1012@gmail.com>
2025-04-01 16:16:43 +0900

    Add files via upload

---
+++ 1_Operator/1_Operator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+            Console.WriteLine(a + b); // 15
+            Console.WriteLine(a - b); // 5
+            Console.WriteLine(a * b); // 50
+            Console.WriteLine(a / b); // 2
+            Console.WriteLine(a % b); // 0
+
+            // int c,d에 각각 20과 10을 넣고 5가지 연산을 해봅시다.
+
+            int c = 20;
+            int d = 10;
+            Console.WriteLine(c + d); // 30
+            Console.WriteLine(c - d); // -10
+            Console.WriteLine(c * d); // 200
+            Console.WriteLine(c / d); // 2
+            Console.WriteLine(c % d); // 0
+
+        }
+    }
+}
---
+++ 1_object/1_object/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // object 자료형
+            object 아빠 = 1234;
+            Console.WriteLine(아빠);
+            Console.WriteLine(아빠.GetType());
+
+
+            아빠 = "문자";
+            Console.WriteLine(아빠);
+            Console.WriteLine(아빠.GetType());
+
+
+            object 엄마 = "엄마, 아빠 보고싶어요";
+            Console.WriteLine(엄마);
+            Console.WriteLine(엄마.GetType());
+
+            // dynamic 자료형 예제
+            dynamic 할아버지 = 1234;
+            Console.WriteLine(할아버지);
+            Console.WriteLine(할아버지.GetType());
+
+            할아버지 = "문자열";
+            Console.WriteLine(할아버지);
+            Console.WriteLine(할아버지.GetType());
+
+            dynamic 할머니 = "할아버지, 할머니 보고싶어";
+            Console.WriteLine(할머니);
+            Console.WriteLine(할머니.GetType());
+
+        }
+    }
+}
